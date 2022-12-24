6253b166731b5a875120214211b6804e231450fa
Pavel Sergeev <p_sergee@mail.ru>
2022-12-24 13:32:41 +0300

    Add break timer script and config.

---
+++ break_timer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+SCRIPT_DIR=`dirname $0`
+source $SCRIPT_DIR/break_timer.cfg
+
+waittime=$worktime
+
+while true
+do
+    sleep $waittime
+    answer=$(zenity --info --text='<span font="26" foreground="Green">Time for a break!</span>' \
+        --extra-button "Snooze (${snoozetime})" --extra-button "Restart" --modal \
+    )
+    if [[ $answer == "" ]]; then
+        sleep $breaktime
+        zenity --info --text='<span font="26" foreground="Green">Time to focus.</span>' --modal
+        waittime=$worktime
+        continue
+    elif [[ $answer == Snooze* ]]; then # Snooze button.
+        waittime=$snoozetime
+        continue
+    else # Restart button. Restarts focus period.
+        waittime=$worktime
+        continue
+    fi
+done
